73236e0f6907d67850f5c79c95b9429743f3504c
Nikita Kozlov <nikita.kozlov@p2p.org>
2022-10-17 14:39:26 +0300

    feat: added workflow to publish to npm

--- lib/ABIProviderLocal.d.ts
+++
@@ -1,5 +0,0 @@
-import { ABIProvider } from './ABIProvider';
-import { ABIElement } from './types';
-export declare class ABIProviderLocal extends ABIProvider {
-    constructor(abiByAddress: Record<string, ABIElement[]>);
-}
--- lib/EVMScriptParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { EVMScriptParsed } from './types';
-export declare const EVMScriptParser: {
-    parse(evmScript: string): {
-        specId: string;
-        calls: EVMScriptParsed[];
-    };
-};
